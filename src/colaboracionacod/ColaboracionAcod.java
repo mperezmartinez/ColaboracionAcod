4a70f0b18ffd15c9b77d935bb5ec625fb1aa3975
Miguel <Miguel@Mipema>
2015-01-29 09:13:20 +0100

    Commit 2

--- src/colaboracionacod/ColaboracionAcod.java
+++ src/colaboracionacod/ColaboracionAcod.java
@@ -17,6 +17,7 @@ public class ColaboracionAcod {
     public static void main(String[] args) {
       
         System.out.println("Hola Mundo");
+        System.out.println("Adios Mundo");
         
         
     }
